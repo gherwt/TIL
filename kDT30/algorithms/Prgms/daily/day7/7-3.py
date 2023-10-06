a8a48bbd0a9cf2e64bdaaab88cddb8ef9949cefa
Lee Sang Min <gherwt46@gmail.com>
2023-10-06 17:19:46 +0900

    TIL

---
+++ kDT30/algorithms/Prgms/daily/day7/7-3.py
@@ -0,0 +1,13 @@
+# def solution(a, b, c, d):
+#     numbers = [a, b, c, d]
+
+# 리스트로 만들고 set 실시 만약 길이가 1 이면 남은 숫자 곱하기 1111
+# 4이면 min 실시해서 도출
+# 만약 2일 경우 두 숫자를 더하고 빼서 절대값하고 곱해줌
+# 만약 3일 경우는
+
+    # print(solution(2, 2, 2, 2))  # 2222
+    # print(solution(4, 1, 4, 4))  # 1681
+    # print(solution(6, 3, 6, 3))  # 27
+    # print(solution(2, 5, 2, 6))  # 30
+    # print(solution(6, 4, 2, 5))  # 2
