aea4b58219488fac91cea2f9c9ebe642ba3a06fc
Lee Sang Min <gherwt46@gmail.com>
2023-10-06 19:27:58 +0900

    today solv

--- kDT30/algorithms/Prgms/daily/day7/7-3.py
+++ kDT30/algorithms/Prgms/daily/day7/7-3.py
@@ -1,13 +1,31 @@
-# def solution(a, b, c, d):
-#     numbers = [a, b, c, d]
+def solution(a, b, c, d):
+    nums = [a, b, c, d]
+    counts = [nums.count(i) for i in nums]
+    if max(counts) == 4:
+        return a * 1111
+    elif max(counts) == 3:
+        p = nums[counts.index(3)]  # 특정 값의 index 를 확인한다.
+        q = nums[counts.index(1)]  # 특정 값의 index 를 확인한다.
+        return (10 * p + q) ** 2
+    elif max(counts) == 2:
+        if min(counts) == 2:
+            if a == b:
+                return (a + c) * abs(a - c)  # counts 값이 2인게 2개이므로 확인을 해줘야 한다.
+            else:
+                (a + b) * abs(a - b)
+        else:
+            q = nums[counts.index(1)]
+            nums.remove(q)
+            counts.remove(1)
+            r = nums[counts.index(1)]
+            # 최소 값이 2가 아니면 최대 값이 2인 경우이다. 이 경우, 3개의 숫자가 있기 때문에 p, q, r 이 존재한다.
+            return q*r
+    else:
+        return min(nums)
 
-# 리스트로 만들고 set 실시 만약 길이가 1 이면 남은 숫자 곱하기 1111
-# 4이면 min 실시해서 도출
-# 만약 2일 경우 두 숫자를 더하고 빼서 절대값하고 곱해줌
-# 만약 3일 경우는
 
-    # print(solution(2, 2, 2, 2))  # 2222
-    # print(solution(4, 1, 4, 4))  # 1681
-    # print(solution(6, 3, 6, 3))  # 27
-    # print(solution(2, 5, 2, 6))  # 30
-    # print(solution(6, 4, 2, 5))  # 2
+# print(solution(2, 2, 2, 2))  # 2222
+# print(solution(4, 1, 4, 4))  # 1681
+# print(solution(6, 3, 6, 3))  # 27
+print(solution(2, 5, 2, 6))  # 30
+# print(solution(6, 4, 2, 5))  # 2
